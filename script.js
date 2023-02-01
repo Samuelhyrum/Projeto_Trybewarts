d6beb7825889800718960a736a9e50cd1fed826a
Samuel Hyrum <samuelhymp@gmail.com>
2023-01-31 21:27:03 -0300

    Subindo tudo

--- script.js
+++ script.js
@@ -28,9 +28,28 @@ function limite_textarea(valor) {
   const quant = 500;
   let total = valor.length;
   if (total <= quant) {
-    resto = quant - total;
+   let resto = quant - total;
     document.getElementById('counter').innerHTML = resto;
   } else {
     document.getElementById('textarea').value = valor.substr(0, quant);
   }
 }
+
+function form(event){
+  event.preventDefault();
+  document.querySelector('#evaluation-form').style.visibility = 'hidden';
+  // document.querySelector('form-data').display = 'flex';
+  const form = document.getElementById('form-data');
+  form.style.display = 'flex';
+  const nome = document.getElementById('input-name');
+  let name = document.createElement('text');
+  form.appendChild(name);
+  name.innerHTML=nome.value;
+  const sobrenome= document.getElementById('input-lastname').value;
+  const email= document.getElementById('input-email').value;
+  const select = document.getElementById('house');
+  // const casa = select.options[select.selectIndex].value;
+  const family = document.querySelector('[name="family"]:checked');
+  const nomeF = (family) ? family.value : '';
+}
+submitBtn.addEventListener('click',form);
